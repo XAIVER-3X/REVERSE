ee87327d8e1204b8ac66e489c515b6cd6b229037
MR-XAIVER <161110098+XAIVER-3X@users.noreply.github.com>
2024-03-08 18:29:27 +0600

    Add files via upload

---
+++ fuck_Enc.py
@@ -0,0 +1,169 @@
+""" REVERSE BY ---( AWM OFFICIAL )--- """
+import os
+import requests
+import marshal
+import zlib
+import base64
+A = '\x1b[1;97m'
+Y = '\x1b[1;33m'
+G = '\x1b[1;96m'
+R = '\x1b[38;5;196m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;48m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X5 = '\x1b[38;5;121m'
+logo = []['\n\n\x1b[1;92m \x1b[38;5;46m  ███████ ███    ██  ██████      ██   ██ \n\x1b[1;92m \x1b[38;5;47m  ██      ████   ██ ██            ██ ██  \n\x1b[1;92m \x1b[38;5;48m  █████   ██ ██  ██ ██      ████   ███   \n\x1b[1;92m \x1b[38;5;49m  ██      ██  ██ ██ ██            ██ ██   \n\x1b[1;92m \x1b[38;5;50m  ███████ ██   ████  ██████      ██   ██ \n '][f'''{X5}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['DEVELOPER   '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['MIKU DEV\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['GITHUB      '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['MikuuXD\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['VERSION     '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['1.0.5\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']["TOOL'S NAME "][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['[\x1b\x1b[38;5;196m\x1b[1;97m\x1b[1;41mPYTHON ENCRYPTION\x1b[0m'][f'''{G3}'''][']\n '][f'''{X5}''']([]['\n\n\x1b[1;92m \x1b[38;5;46m  ███████ ███    ██  ██████      ██   ██ \n\x1b[1;92m \x1b[38;5;47m  ██      ████   ██ ██            ██ ██  \n\x1b[1;92m \x1b[38;5;48m  █████   ██ ██  ██ ██      ████   ███   \n\x1b[1;92m \x1b[38;5;49m  ██      ██  ██ ██ ██            ██ ██   \n\x1b[1;92m \x1b[38;5;50m  ███████ ██   ████  ██████      ██   ██ \n '][f'''{X5}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['DEVELOPER   '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['MIKU DEV\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['GITHUB      '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['MikuuXD\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']['VERSION     '][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['1.0.5\n'][f'''{X2}''']['【'][f'''{X5}''']['✦'][f'''{X2}''']['】'][f'''{Y}''']["TOOL'S NAME "][f'''{B}''']['»----'][f'''{A}''']['➤ '][f'''{G3}''']['[\x1b\x1b[38;5;196m\x1b[1;97m\x1b[1;41mPYTHON ENCRYPTION\x1b[0m'][f'''{G3}'''][']\n '][f'''{X5}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'])
+
+def U7P4L():
+    os.system('clear')
+    os.system('xdg-open https://github.com/MikuuXD ')
+    os.system('clear')
+    print(logo)
+    print(f'''{B}【{A}01{B}】{G2}ENCRYPT BASE16''')
+    print(f'''{B}【{A}02{B}】{G2}ENCRYPT BASE32''')
+    print(f'''{B}【{A}03{B}】{G2}ENCRYPT BASE64 ''')
+    print(f'''{B}【{A}04{B}】{G2}ENCRYPT MARSHAL''')
+    print(f'''{B}【{A}05{B}】{G2}ENCRYPT MARSHAL-ZLIB-BASE16''')
+    print(f'''{B}【{A}06{B}】{G2}ENCRYPT MARSHAL-ZLIB-BASE32''')
+    print(f'''{B}【{A}07{B}】{G2}ENCRYPT MARSHAL-ZLIB-BASE64''')
+    print(f'''{B}【{A}08{B}】{G1}FIND MORE TOOLS ''')
+    print(f'''{B}【{A}00{B}】{R}EXIT PROGRAMME ''')
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+    u7p4l = input(f'''{B}【{A}•{B}】{G}Pilih {A}➤\x1b[1;32m ''')
+    if u7p4l in ('1', '01', 'A', 'a'):
+        os.system('clear')
+        print(logo)
+        x1()
+        return None
+    if None in ('2', '02', 'B', 'b'):
+        os.system('clear')
+        print(logo)
+        x2()
+        return None
+    if None in ('3', '03', 'C', 'c'):
+        os.system('clear')
+        print(logo)
+        x3()
+        return None
+    if None in ('4', '04', 'D', 'd'):
+        os.system('clear')
+        print(logo)
+        x4()
+        return None
+    if None in ('5', '05', 'E', 'e'):
+        os.system('clear')
+        print(logo)
+        x5()
+        return None
+    if None in ('6', '06', 'F', 'f'):
+        os.system('clear')
+        print(logo)
+        x6()
+        return None
+    if None in ('7', '07', 'G', 'g'):
+        os.system('clear')
+        print(logo)
+        x7()
+        return None
+    if None in ('8', '08', 'H', 'h'):
+        os.system('xdg-open https://github.com/MikuuXD')
+        menu()
+        return None
+    if None in ('0', '00', 'i', 'I'):
+        exit(f'''{B}【{A}={B}】{G}EXIT DONE \n\n''')
+        return None
+    None(f'''{B}【{A}={B}】{G}OPTION NOT FOUND IN MENU...''')
+    U7P4L()
+
+
+def x1():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    ups = open(u7p4l, 'rb').read()
+    ui = base64.b16encode(ups)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    cok = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nmport base64\nexec(base64.b16decode(' + str(ui) + '))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X2}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x2():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    ups = open(u7p4l, 'rb').read()
+    ui = base64.b32encode(ups)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    cok = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport base64\nexec(base64.b32decode(' + str(ui) + '))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X1}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x3():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    ups = open(u7p4l, 'rb').read()
+    ui = base64.b64encode(ups)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    cok = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport base64\nexec(base64.b64decode(' + str(ui) + '))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X2}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x4():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    fileopen = open(u7p4l, 'rb').read()
+    a = compile(fileopen, 'dg', 'exec')
+    m = marshal.dumps(a)
+    s = repr(m)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    d = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport marshal\nexec(marshal.loads(' + s + '))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{Xyz}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x5():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    fileopen = open(u7p4l, 'rb').read()
+    a = compile(fileopen, 'dg', 'exec')
+    m = marshal.dumps(a)
+    z = zlib.compress(m)
+    b = base64.b16encode(z)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    d = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport marshal,zlib,base64\nexec(marshal.loads(zlib.decompress(base64.b16decode(' + str(b) + '))))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X2}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x6():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    fileopen = open(u7p4l, 'rb').read()
+    a = compile(fileopen, 'dg', 'exec')
+    m = marshal.dumps(a)
+    z = zlib.compress(m)
+    b = base64.b32encode(z)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    d = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport marshal,zlib,base64\nexec(marshal.loads(zlib.decompress(base64.b32decode(' + str(b) + '))))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X2}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def x7():
+    u7p4l = input(f'''\n{B}【{A}={B}】{G1}Masukkan Sorce Mu {A}➤{G} ''')
+    fileopen = open(u7p4l, 'rb').read()
+    a = compile(fileopen, 'dg', 'exec')
+    m = marshal.dumps(a)
+    z = zlib.compress(m)
+    b = base64.b64encode(z)
+    output = u7p4l.replace('.py', '') + '_enc.py'
+    d = open(output, 'w').write('#Compile By Miku Dev\n#Jangan_Lupa_Follow_Github_Gw_Yah_Tod\n#Mau Recode?\n#Bayar Dulu 1juta\n\nimport marshal,zlib,base64\nexec(marshal.loads(zlib.decompress(base64.b64decode(' + str(b) + '))))')
+    print(f'''{B}【{A}={B}】{X1}File Telah Di Enc {A}➤{Y} %s''' % u7p4l)
+    print(f'''{B}【{A}={B}】{X2}File Di Simpan Ke{A}➤{Y} %s''' % output)
+    print(f''' {X5}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+U7P4L()
